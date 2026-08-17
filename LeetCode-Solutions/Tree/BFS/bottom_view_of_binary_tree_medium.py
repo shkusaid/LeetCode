a22fb42f9ca52c799a63c9f754d3a3a6557e6034
shkusaid <shkusaid910@gmail.com>
2026-08-17 15:53:06 +0500

    Adding Bottom View

---
+++ LeetCode-Solutions/Tree/BFS/bottom_view_of_binary_tree_medium.py
@@ -0,0 +1,30 @@
+from collections import deque
+
+class TreeNode:
+    def __init__(self, data):
+        self.val = data
+        self.left = None
+        self.right = None
+
+def bottom_view(root):
+    if root is None:
+        return []
+    q = deque([(root , 0)])
+    tree_map = {}
+    while q:
+        node , v = q.popleft()
+        tree_map[v] = node.val
+        if node.left:
+            q.append((node.left , v - 1))
+        if node.right:
+            q.append((node.right , v + 1))
+    return [tree_map[v] for v in sorted(tree_map)]
+
+root = TreeNode(1)
+root.left = TreeNode(2)
+root.right = TreeNode(3)
+root.left.left = TreeNode(4)
+root.left.right = TreeNode(5)
+root.right.right = TreeNode(6)
+
+print("Top View:", bottom_view(root))
