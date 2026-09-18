066dd69a2479be50eb1b56b80c0719f2b348e1c6
shkusaid <shkusaid910@gmail.com>
2026-09-18 18:00:01 +0500

    next permutations

---
+++ LeetCode-Solutions/Array/Greedy/next_permutations_medium_31.py
@@ -0,0 +1,15 @@
+def next_permutations(nums):
+    n = len(nums)
+    i = n - 2
+    while i >= 0 and nums[i] >= nums[i + 1]:
+        i -= 1
+    if i >= 0:
+        j = n - 1
+        while nums[j] <= nums[i]:
+            j -= 1
+        nums[i] , nums[j] = nums[j] , nums[i]
+    nums[i + 1:] = nums[i + 1:][::-1]
+    return nums
+
+nums = [1,2,3]
+print(next_permutations(nums))
